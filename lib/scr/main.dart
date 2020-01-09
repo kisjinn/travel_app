110334f57c62c03d525cf4cee0e63dca89da02cd
kisjinn <sakshijinnewar@gmail.com>
2020-01-09 21:40:25 +0530

    Initial commit

---
+++ lib/scr/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/scr/screens/welcome.dart';
+
+void main()=> runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Welcome(),
+));
